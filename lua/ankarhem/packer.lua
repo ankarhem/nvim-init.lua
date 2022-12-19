3cfe9305f7db537fbfcb48fa9fbba9baa2e9c5bf
Jakob Ankarhem <jakob.ankarhem@outlook.com>
2022-12-19 19:43:11 +0100

    add hop

--- lua/ankarhem/packer.lua
+++ lua/ankarhem/packer.lua
@@ -8,18 +8,18 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    -- or                            , branch = '0.1.x',
+    requires = { { 'nvim-lua/plenary.nvim' } }
   }
 
   -- Install tokyonight and apply theme
-  use ({
-	  'folke/tokyonight.nvim',
-	  config = function()
-		  vim.cmd('colorscheme tokyonight-storm')
+  use({
+    'folke/tokyonight.nvim',
+    config = function()
+      vim.cmd('colorscheme tokyonight-storm')
 
-	  end
+    end
   })
 
   use({ 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' })
@@ -29,25 +29,25 @@ return require('packer').startup(function(use)
   use('tpope/vim-fugitive')
 
   use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  requires = {
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},
-		  {'williamboman/mason.nvim'},
-		  {'williamboman/mason-lspconfig.nvim'},
+    'VonHeikemen/lsp-zero.nvim',
+    requires = {
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' },
+      { 'williamboman/mason.nvim' },
+      { 'williamboman/mason-lspconfig.nvim' },
 
-		  -- Autocompletion
-		  {'hrsh7th/nvim-cmp'},
-		  {'hrsh7th/cmp-buffer'},
-		  {'hrsh7th/cmp-path'},
-		  {'saadparwaiz1/cmp_luasnip'},
-		  {'hrsh7th/cmp-nvim-lsp'},
-		  {'hrsh7th/cmp-nvim-lua'},
+      -- Autocompletion
+      { 'hrsh7th/nvim-cmp' },
+      { 'hrsh7th/cmp-buffer' },
+      { 'hrsh7th/cmp-path' },
+      { 'saadparwaiz1/cmp_luasnip' },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'hrsh7th/cmp-nvim-lua' },
 
-		  -- Snippets
-		  {'L3MON4D3/LuaSnip'},
-		  {'rafamadriz/friendly-snippets'},
-	  }
+      -- Snippets
+      { 'L3MON4D3/LuaSnip' },
+      { 'rafamadriz/friendly-snippets' },
+    }
   }
 
   use("folke/zen-mode.nvim")
@@ -60,14 +60,21 @@ return require('packer').startup(function(use)
 
   -- Comment toggler
   use({
-      'numToStr/Comment.nvim',
-      config = function()
-          require('Comment').setup()
-      end
+    'numToStr/Comment.nvim',
+    config = function()
+      require('Comment').setup()
+    end
   })
 
   use({
-      'nmac427/guess-indent.nvim',
-      config = function() require('guess-indent').setup {} end,
+    'nmac427/guess-indent.nvim',
+    config = function() require('guess-indent').setup {} end,
   })
+
+
+  use({
+    'phaazon/hop.nvim',
+    branch = 'v2', -- optional but strongly recommended
+  })
+
 end)
